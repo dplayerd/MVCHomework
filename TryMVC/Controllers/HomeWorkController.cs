158ff6990f3fc79902ce6e3d55bd8b7fc2a8c16d
Moudou <will@kingfor.com>
2020-02-14 09:27:08 +0800

    【調整】改用 EF 讀值

--- TryMVC/Controllers/HomeWorkController.cs
+++ TryMVC/Controllers/HomeWorkController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TryMVC.Services;
 using TryMVC.ViewModels;
 
 namespace TryMVC.Controllers
@@ -19,33 +20,34 @@ namespace TryMVC.Controllers
         [ChildActionOnly]
         public ActionResult ListPage()
         {
-            List<HomeworkViewModel> list = new List<HomeworkViewModel>();
-            Random random = new Random(DateTime.Now.Millisecond);
+            //List<HomeworkViewModel> list = new List<HomeworkViewModel>();
+            //Random random = new Random(DateTime.Now.Millisecond);
 
 
-            for (var i = 0; i < 100; i++)
-            {
-                var y = DateTime.Now.Year;
-                var month = DateTime.Now.Month;
-                var d = random.Next(1, 30);
-                var h = random.Next(0, 23);
-                var m = random.Next(0, 59);
-                var amount = random.Next(1, 100) * 100;
+            //for (var i = 0; i < 100; i++)
+            //{
+            //    var y = DateTime.Now.Year;
+            //    var month = DateTime.Now.Month;
+            //    var d = random.Next(1, 30);
+            //    var h = random.Next(0, 23);
+            //    var m = random.Next(0, 59);
+            //    var amount = random.Next(1, 100) * 100;
 
-                var type = random.Next(0, 2);
+            //    var type = random.Next(0, 2);
 
-                list.Add(new HomeworkViewModel()
-                {
-                    ID = i,
-                    Name = "Name" + i,
-                    Type = type,
-                    TypeName = (type == 0) ? "收入" : "支出",
-                    CreateDate = new DateTime(y, month, d, h, m, 0),
-                    Amount = amount
-                });
-            }
+            //    list.Add(new HomeworkViewModel()
+            //    {
+            //        ID = i,
+            //        Name = "Name" + i,
+            //        Type = type,
+            //        TypeName = (type == 0) ? "收入" : "支出",
+            //        CreateDate = new DateTime(y, month, d, h, m, 0),
+            //        Amount = amount
+            //    });
+            //}
 
 
+            var list = new HomeworkService().GetList();
             list = list.OrderBy(item => item.CreateDate).ToList();
 
             return View(list);
---
+++ TryMVC/Services/HomeworkService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TryMVC.ViewModels;
+using TryMVC.ORM;
+
+namespace TryMVC.Services
+{
+    public class HomeworkService
+    {
+        public List<HomeworkViewModel> GetList()
+        {
+            var context = new HomeworkDBEntities();
+
+            int i = 0;
+            var list =
+                (from item in context.AccountBook
+                 select new HomeworkViewModel()
+                 {
+                     Amount = item.Amounttt,
+                     Name = item.Remarkkk,
+                     Type = item.Categoryyy,
+                     TypeName = (item.Categoryyy == 0) ? "收入" : "支出",
+                     CreateDate = DateTime.Now
+                 }).ToList();
+
+
+            return list;
+        }
+    }
+}
